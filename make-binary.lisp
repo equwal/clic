6d56c6a8bd6e69ad1bda2c38c6b90f3540744a41
Solene Rapenne <solene@perso.pw>
2017-11-03 17:03:55 +0000

    sbcl can produce standalone binaries

--- make-binary.lisp
+++ make-binary.lisp
@@ -1,12 +1,24 @@
+;; ecl produces a linked binary
+;; while sbcl produces a static binary (but huge ~ 10Mb)
+
+#+ecl
 (require 'cmp)
+#+ecl
 (require 'sockets)
-
-(loop for file in '("clic" "clic.o")
-      do (and (probe-file file) (delete-file file)))
-
-(compile-file "clic.lisp" :system-p t)
-(c:build-program "clic" :lisp-files '("clic.o"))
-(delete-file "clic.o")
+#+ecl
+(progn
+  (compile-file "clic.lisp" :system-p t)
+  (c:build-program "clic" :epilogue-code '(progn (start)) :lisp-files '("clic.o")))
+  ;;(c:build-program "clic" :lisp-files '("clic.o")))
+#+sbcl
+(progn
+  (require 'sb-bsd-sockets)
+  (sb-ext:disable-debugger)
+  (load "clic.lisp")
+  (sb-ext:save-lisp-and-die "clic"
+			    :executable t
+			    :compression 5
+			    :toplevel 'start))
 
 (format t "INFO => Compilation done (or at least it should)~%")
 (quit)
