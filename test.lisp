16c4a85655da1d9cc8a9dd2dadd15e563b7546de
Solene Rapenne <solene@perso.pw>
2017-11-04 16:14:17 +0000

    Tests for history and invalid links

--- test.lisp
+++ test.lisp
@@ -1,10 +1,16 @@
 ;; we can write scenario here
 
+(p)
+(g 2)
 (getpage "bitreich.org" 70 "/")
 (g 11) ;; going to radio
 (g 35) ;; going back
 (g 55) ;; banana !
 (g 0) ;; going back
+(p)
+(p)
+
+(print *history*)
 
 
 (quit)
