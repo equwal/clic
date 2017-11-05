e68ec9c78ce9eebcffa1fd18717c80115abe1f34
Solene Rapenne <solene@perso.pw>
2017-11-05 17:10:51 +0000

    more test for url argument

--- run-test.sh
+++ run-test.sh
@@ -5,5 +5,8 @@ LISP=$1
 ${LISP} --load clic.lisp --load test.lisp
 
 ./clic gopher://bitreich.org:70/0/  | md5sum -
+./clic bitreich.org:70/0/  | md5sum -
+./clic bitreich.org/0/  | md5sum -
 echo "/" | nc bitreich.org 70 | md5sum -
+
 
