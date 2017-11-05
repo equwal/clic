4c947fdfb4a057cb6e99ac41d836369a415c6138
Solene Rapenne <solene@perso.pw>
2017-11-05 16:54:28 +0000

    add test for command line parsing

--- test.lisp
+++ test.lisp
@@ -1,5 +1,11 @@
 ;; we can write scenario here
 
+(print (parse-url "gopher://perso.pw:70/0/"))
+(print (parse-url "gopher://perso.pw/0/a"))
+(print (parse-url "perso.pw/0/some/uri.txt"))
+(print (parse-url "perso.pw"))
+(print (parse-url "perso.pw:70"))
+
 (p)
 (g 2)
 (getpage "bitreich.org" 70 "/")
