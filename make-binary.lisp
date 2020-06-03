38eb1e4a49bd594bd6108c0beec5fb3afa09e8fd
Solene Rapenne <solene@perso.pw>
2020-06-03 10:36:35 +0200

    Fix compilation for ecl 20.4.24

--- make-binary.lisp
+++ make-binary.lisp
@@ -1,6 +1,10 @@
 (load "3rdparties/bundle.lisp")
 (require :asdf)
 
+;; added since ecl update otherwise ecl doesn't compile them
+(require :usocket)
+(require :cl+ssl)
+
 ;; load clic which is in $PWD
 (push '*default-pathname-defaults* asdf:*central-registry*)
 
@@ -9,7 +13,10 @@
 (asdf:make-build "clic" :type :program
                  :monolithic t
                  :move-here "."
-                 :prologue-code '(ext:set-signal-handler ext:+sigint+ nil)
+                 :prologue-code '(progn 
+				   (ext:set-signal-handler ext:+sigint+ nil)
+				   (require :asdf)
+				   (require :sb-bsd-sockets))
                  :epilogue-code '(progn (handler-case (main)
                                           (condition () (quit)))))
 (quit)
